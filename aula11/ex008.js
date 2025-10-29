2d50d581275a5b9ef2b3b53a9703551135dbd363
KauanNaves <kauannazanaves0@gmail.com>
2025-10-29 12:30:16 -0300

    Create ex008, 009, 010 and 011

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 150
+console.log(`A velocidade do seu carro é: ${velocidade} Km/h`)
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+if (velocidade > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
